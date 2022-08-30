ab401313689d8c1671e3a6af94e93d58ee26dfb9
tsingcbx99 <57670068+tsingcbx99@users.noreply.github.com>
2022-08-30 17:01:55 +0800

    Results with different proportions

--- scripts/exp.sh
+++ scripts/exp.sh
@@ -6,6 +6,19 @@
 python train_DRAEM.py --data_path datasets/mvtec --obj_id 2 --sample_rate 1.0 --anomaly_source_path datasets/dtd/images \
   --gpu_id 0 --lr 0.0001 --bs 6 --epochs 100 --log_path logs/carpet_sp_100
 
+# Ratio 100% Image Auc 100 Pixel AP 66.3
+# Ratio 50% Image Auc 99.3 Pixel AP 61.7
+# Ratio 30% Image Auc 99.8 Pixel AP 64.0
+# Ratio 10% Image Auc 99.6 Pixel AP 53.0
+python train_DRAEM.py --data_path datasets/mvtec --obj_id 13 --sample_rate 1.0 --anomaly_source_path datasets/dtd/images \
+  --gpu_id 0 --lr 0.0001 --bs 6 --epochs 100 --log_path logs/grid_sp_100
+python train_DRAEM.py --data_path datasets/mvtec --obj_id 13 --sample_rate 0.5 --anomaly_source_path datasets/dtd/images \
+  --gpu_id 1 --lr 0.0001 --bs 6 --epochs 200 --log_path logs/grid_sp_50
+python train_DRAEM.py --data_path datasets/mvtec --obj_id 13 --sample_rate 0.3 --anomaly_source_path datasets/dtd/images \
+  --gpu_id 2 --lr 0.0001 --bs 6 --epochs 300 --log_path logs/grid_sp_30
+python train_DRAEM.py --data_path datasets/mvtec --obj_id 13 --sample_rate 0.1 --anomaly_source_path datasets/dtd/images \
+  --gpu_id 3 --lr 0.0001 --bs 6 --epochs 1000 --log_path logs/grid_sp_10
+
 # joint training
 
 # TODO why not using standard augmentations
